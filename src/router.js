aea88ad16c4375e4ce7cafccf979da4383f1487e
Tenisson Chaves Jr <MY_tenisson.jr@gmail.com>
2021-07-23 20:54:55 -0300

    Async Components-Routes e deploy no Firebase

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-import CoachDetail from './pages/coaches/CoachDetail.vue';
+//import CoachDetail from './pages/coaches/CoachDetail.vue';
 import CoachesList from './pages/coaches/CoachesList.vue';
 import CoachRegistation from './pages/coaches/CoachRegistration.vue';
 import ContactCoach from './pages/requests/ContactCoach.vue';
@@ -8,6 +8,8 @@ import RequestsReceived from './pages/requests/RequestsReceived.vue';
 import NotFound from './pages/NotFound.vue';
 import UserAuth from './pages/auth/UserAuth.vue';
 import store from './store/index.js';
+
+const CoachDetail = () => import('./pages/coaches/CoachDetail.vue');
 
 const router = createRouter({
   history: createWebHistory(),
